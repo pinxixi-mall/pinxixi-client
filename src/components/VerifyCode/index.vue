44710dc3e726bac07781bd0ed5a0ed179584bd81
yetm <yetm@ibumobile.com>
2022-02-16 10:35:23 +0800

    add:login verify code

--- src/components/VerifyCode/index.vue
+++ src/components/VerifyCode/index.vue
@@ -1,22 +1,27 @@
 <template>
-  <div>
+  <div @click="handleClick">
     <canvas ref="verifyCodeRef" :width="width" :height="height"></canvas>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, defineProps } from 'vue'
+import { onMounted, ref, defineProps, defineEmit } from 'vue'
 
 const props = defineProps({
+  modelValue: null,
   width: Number,
   height: Number,
   codeLength: Number
 })
 
-const { width = 110, height = 46, codeLength = 4 } = props
+const emit = defineEmit(["update:modelValue"])
+
+const { width = 110, height = 40, codeLength = 4 } = props
 
 const verifyCodeRef = ref(null)
-const texts = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+const TEXTS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+const LINES = 6
+const DOTS = 50
 
 let ctx
 
@@ -25,41 +30,79 @@ onMounted(() => {
   drawCodeImg()
 })
 
+const handleClick = () : void => {
+  drawCodeImg()
+}
+
 // 绘制验证码
 const drawCodeImg = () : void => {
   // 绘制背景
   ctx.fillStyle = randomColor(200, 230)
   ctx.fillRect(0, 0, width, height)
-  // ctx.save()
 
   drawText()
+  drawLine()
+  drawDot()
 }
+
+// 随机数
+const randomNum = (min, max): number => Math.floor(Math.random() * (max - min) + min)
 
 // 随机颜色
 const randomColor = (min: number, max: number) : string => {
   return `rgb(${randomNum(min, max)}, ${randomNum(min, max)}, ${randomNum(min, max)})`
 }
 
-// 随机数
-const randomNum = (min, max): number => Math.floor(Math.random() * (max - min) + min)
-
 // 绘制文本
 const drawText = (): void => {
+  let code = ''
   for (let index = 0; index < codeLength; index++) {
-    const textIdx = randomNum(0, texts.length)
-    console.log('------log------', texts[textIdx])
-    ctx.font = '30px'
-    ctx.fillText(texts[textIdx], 50, 20)
+    const textIdx = randomNum(0, TEXTS.length)
+    const deg = randomNum(-40, 40) // 旋转角度
+    const y = randomNum(22, 36) // y方向坐标
+    const fontSize = randomNum(20, 30) // 字体大小
+    const letter = TEXTS[textIdx]
+    ctx.textBaseline = 'alphabetic'
+    ctx.font = `${fontSize}px Simhei`
+    ctx.fillStyle = randomColor(60, 150)
+    ctx.save()
+    ctx.translate(10 + index * 26, y)
+    ctx.rotate(deg * Math.PI / 180)
+    ctx.fillText(letter, 0, 0)
+    ctx.restore()
+    code += letter
   }
+
+  emit("update:modelValue", code)
 }
 
 // 绘制线
 const drawLine = (): void => {
+  for (let index = 0; index < LINES; index++) {
+    const x1 = randomNum(0, ctx.canvas.clientWidth)
+    const y1 = randomNum(0, ctx.canvas.clientHeight)
+    const x2 = randomNum(0, ctx.canvas.clientWidth)
+    const y2 = randomNum(0, ctx.canvas.clientHeight)
+    ctx.beginPath()
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.lineWidth = 1
+    ctx.strokeStyle = randomColor(150, 250)
+    ctx.stroke()
+  }
+}
 
+// 绘制点
+const drawDot = (): void => {
+  for (let index = 0; index < DOTS; index++) {
+    const x = randomNum(0, ctx.canvas.clientWidth)
+    const y = randomNum(0, ctx.canvas.clientHeight)
+    const r = randomNum(1, 2)
+    ctx.beginPath()
+    ctx.arc(x, y, r, 0, 2 * Math.PI)
+    ctx.fillStyle = randomColor(100, 200)
+    ctx.fill()
+  }
 }
 
 </script>
-
-<style scoped>
-
-</style>
--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -1,31 +1,33 @@
 <template>
   <div class="page">
-    <van-image round class="logo" :src="logo" />
+    <!-- <van-image round class="logo" :src="logo" /> -->
     <van-form @submit="onSubmit" class="login-form" ref="formRef">
       <van-field
         v-model="username"
         name="username"
         label="用户名"
-        placeholder="请填写用户名"
-        :rules="[{ required: true, message: '请填写用户名' }]"
+        placeholder="请输入"
+        :rules="[{ required: true, message: '请输入用户名' }]"
       />
       <van-field
         v-model="password"
         type="password"
         name="password"
         label="密码"
-        placeholder="请填写密码"
-        :rules="[{ required: true, message: '请填写密码' }]"
+        placeholder="请输入"
+        :rules="[{ required: true, message: '请输入密码' }]"
       />
       <van-field
         v-model="verifyCode"
         center
         clearable
         label="验证码"
-        placeholder="请输入验证码"
+        placeholder="请输入"
+        :rules="[{ required: true, message: '请输入验证码' }, 
+        { validator: checkVerifyCode, message: '验证码不正确'}]"
       >
         <template #button>
-          <VerifyCode />
+          <VerifyCode v-model="codeImgText" />
         </template>
       </van-field>
       <div class="submit-btn">
@@ -53,6 +55,7 @@ const Login = {
   setup() {
     const router = useRouter()
     const formRef = ref(null)
+    const codeImgText = ref(null)
     const state = reactive({
       username: null,
       password: null,
@@ -81,12 +84,17 @@ const Login = {
       router.push('register')
     }
 
+    // 验证码校验
+    const checkVerifyCode = (val) => val.toLowerCase() === codeImgText.value.toLowerCase()
+
     return {
       logo,
       ...toRefs(state),
       onSubmit,
       formRef,
-      onRegister
+      onRegister,
+      codeImgText,
+      checkVerifyCode
     }
   }
 }
